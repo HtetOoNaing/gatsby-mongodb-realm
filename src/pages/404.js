13cd651cc42398a77089d65377feece29d59b0e4
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2021-11-24 16:10:01 +0630

    connect mongodb realm with annonumous credentials

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NotFound = () => (
+	<div>Your request page is not found</div>
+)
+
+export default NotFound;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import * as Realm from 'realm-web'
 
 export default function Home() {
-  return <div>Hello world!</div>
+	const [countries, setCountries] = useState([]);
+	useEffect(() => {
+		async function fetchData() {
+			const REALM_APP_ID = process.env.REALM_APP_ID;
+			const app = new Realm.App({ id: REALM_APP_ID });
+			const credentials = Realm.Credentials.anonymous();
+			try {
+				const user = await app.logIn(credentials);
+				const allCountries = await user.functions.getAllCountries();
+				console.log(allCountries);
+				setCountries(allCountries)
+			} catch(err) {
+				console.error("Failed to log in", err);
+			}
+		}
+		fetchData()
+	}, [])
+	return <div>Hello world! {process.env.REALM_APP_ID}</div>
 }
